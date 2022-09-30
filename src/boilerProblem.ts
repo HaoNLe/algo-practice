7edafc7fbca150f20ab49bb83edc49178f86759a
haonle <haole@berkeley.edu>
2022-09-30 13:12:33 -0700

    added boiler plate

---
+++ src/boilerProblem.ts
@@ -0,0 +1,52 @@
+import { testCase } from "./types";
+
+/*
+Four steps
+1. Understand question and scope (description, constraints, test cases)
+2. High level and buy-in (pseudo-code, high-level, big(O) space/time)
+3. Implementation deep-dive
+4. Wrap (Review implementation and big(O), refinements if had time, )
+*/
+
+/*
+DESCRIPTION
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+CONSTRAINTS
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+  culpa qui officia deserunt mollit anim id est laborum.
+
+*/
+
+/*
+SOLUTION
+
+General idea
+[TODO]
+
+Base cases
+  1. [TODO]
+
+Pitfalls
+  1. [TODO]
+
+PSEUDOCODE
+  [TODO]
+
+*/
+
+function boiler(): boolean {
+  return true;
+}
+
+export const testCases: testCase[]= [
+  [
+    {},
+    [true]
+  ]
+];
+
+export default boiler;
