14e8fef10339cc13d7eb9ca1d050446e1a0a36c8
haonle <haole@berkeley.edu>
2022-10-05 15:13:08 -0700

    started utility file

---
+++ src/resources/utility.ts
@@ -0,0 +1,11 @@
+import { treeNode } from "./types";
+
+function createBinaryTreeFromArray(treeNodes: number[]): treeNode {
+  if (treeNodes.length === 0) {
+    return new treeNode();
+  }
+
+  return new treeNode();
+}
+
+export default { createBinaryTreeFromArray };
