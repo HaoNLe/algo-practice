60824907f1901193aff505307ea80832a789a9e1
haonle <haole@berkeley.edu>
2022-10-05 15:14:07 -0700

    added tests

---
+++ src/resources/utility.test.ts
@@ -0,0 +1,27 @@
+import utility from './utility'
+
+test('empty array returns empty treeNode', () => {
+  const treeRoot = utility.createBinaryTreeFromArray([]);
+  expect(treeRoot.val).toBeNull();
+});
+
+test('single element array returns single treeNode', () => {
+  const rootVal = 1;
+  const treeRoot = utility.createBinaryTreeFromArray([rootVal]);
+  expect(treeRoot.val).toEqual(rootVal);
+  expect(treeRoot.left).toBeNull();
+  expect(treeRoot.right).toBeNull();
+});
+
+test('three element array returns two-height tree', () => {
+  const rootVal = 1;
+  const leftVal = 2;
+  const rightVal = 3;
+
+  const treeRoot = utility.createBinaryTreeFromArray([rootVal, leftVal, rightVal]);
+  expect(treeRoot.val).toEqual(rootVal);
+  expect(treeRoot.left).toBeTruthy();
+  expect(treeRoot.left?.val).toEqual(leftVal);
+  expect(treeRoot.right).toBeTruthy();
+  expect(treeRoot.right?.val).toEqual(rightVal);
+});
