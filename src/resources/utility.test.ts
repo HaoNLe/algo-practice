21d9930ba681812661eaa9391cf8652c60b43c47
haonle <haole@berkeley.edu>
2022-10-05 15:33:13 -0700

    fix launch.json later. npm test working now

--- src/resources/utility.test.ts
+++ src/resources/utility.test.ts
@@ -2,7 +2,7 @@ import utility from './utility'
 
 test('empty array returns empty treeNode', () => {
   const treeRoot = utility.createBinaryTreeFromArray([]);
-  expect(treeRoot.val).toBeNull();
+  expect(treeRoot.val).toBeUndefined();
 });
 
 test('single element array returns single treeNode', () => {
@@ -17,7 +17,6 @@ test('three element array returns two-height tree', () => {
   const rootVal = 1;
   const leftVal = 2;
   const rightVal = 3;
-
   const treeRoot = utility.createBinaryTreeFromArray([rootVal, leftVal, rightVal]);
   expect(treeRoot.val).toEqual(rootVal);
   expect(treeRoot.left).toBeTruthy();
